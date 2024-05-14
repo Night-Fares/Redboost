1124740a51d452a25f15e4b01b78ce3192eed971
Night-Fares <fares.lassoued@etudiant-isi.utm.tn>
2024-05-14 20:00:45 +0100

    test locl again

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,48 +1,32 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
-import { persistReducer, persistStore } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { configureStore } from '@reduxjs/toolkit'
 import loginSliceReducer from './features/login/loginSlice'
 import registerSliceReducer from './features/register/registerSlice'
 import appReducer from './features/appSlice/appSlice'
 import userDataReducer from './features/userData/userData'
 import eventsReducer from './features/events/events'
 import programsSliceReducer from './features/programs/programsSlice'
-import entrepreneursReducer from './features/entrepreneursData/entrepreneursSlice'
+import entrepreneursReducer from './features/entrepreneursData/entrepreneursSlice' // Add this import
 import activityReducer from './features/activity/activitySlice'
 import usersReducer from './features/users/usersSlice'
 import taskReducer from './features/task/taskSlice'
 import routeReducer from './features/routeSlice/routeSlice'
-import authReducer from './features/auth/authSlice' // Add this
-
-// You may need to use combineReducers if you have multiple reducers
-const rootReducer = combineReducers({
-  app: appReducer,
-  loginSlice: loginSliceReducer,
-  registerSlice: registerSliceReducer,
-  programsSlice: programsSliceReducer,
-  userData: userDataReducer,
-  events: eventsReducer,
-  entrepreneurs: entrepreneursReducer,
-  activity: activityReducer,
-  usersSlice: usersReducer,
-  task: taskReducer,
-  route: routeReducer,
-  auth: authReducer,
-})
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['auth'], // You can add more reducers here that you want to persist
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-// Configure the Redux store with the persisted reducer
+import authReducer from './features/auth/authSlice'
+// Configure the Redux store
 const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    app: appReducer,
+    loginSlice: loginSliceReducer,
+    registerSlice: registerSliceReducer,
+    programsSlice: programsSliceReducer,
+    userData: userDataReducer,
+    events: eventsReducer,
+    entrepreneurs: entrepreneursReducer,
+    activity: activityReducer,
+    usersSlice: usersReducer,
+    task: taskReducer,
+    route: routeReducer,
+    auth: authReducer,
+  },
 })
 
-const persistor = persistStore(store)
-
-export { store, persistor }
+export default store
