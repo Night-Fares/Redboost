288fbec1ce7481b8b992ff98f31be339a1212656
Night-Fares <fares.lassoued@etudiant-isi.utm.tn>
2024-05-15 09:48:16 +0100

    prob

--- backend/routes/api/login.js
+++ backend/routes/api/login.js
@@ -4,32 +4,41 @@ const passport = require("passport");
 const router = express.Router();
 
 router.post("/login", async (req, res, next) => {
-  passport.authenticate("login", async (error, user, info) => {
-    console.log("req user", req.user);
-    console.log("req session", req.session);
-    try {
-      if (error) {
-        return res.status(500).json({
-          message: "Something is wrong logging in",
-          error: error || "internal server errror",
-        });
-      }
-
-      //req.login is provided by passport to serilize user id
-      req.login(user, async (error) => {
+  passport.authenticate(
+    "login",
+    {
+      successRedirect: "/dash",
+      failureRedirect: "/",
+      failureMessage: true,
+      keepSessionInfo: true,
+    },
+    async (error, user, info) => {
+      console.log("req user", req.user);
+      console.log("req session", req.session);
+      try {
         if (error) {
-          res.status(500).json({
+          return res.status(500).json({
             message: "Something is wrong logging in",
             error: error || "internal server errror",
           });
         }
-        req.session.email = user.email;
-        return res.send({ user, info });
-      });
-    } catch (error) {
-      return next(error);
+
+        //req.login is provided by passport to serilize user id
+        req.login(user, async (error) => {
+          if (error) {
+            res.status(500).json({
+              message: "Something is wrong logging in",
+              error: error || "internal server errror",
+            });
+          }
+          req.session.email = user.email;
+          return res.send({ user, info });
+        });
+      } catch (error) {
+        return next(error);
+      }
     }
-  })(req, res, next);
+  )(req, res, next);
 });
 
 module.exports = router;
